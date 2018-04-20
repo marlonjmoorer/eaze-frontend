74d92002d64a02d9449f3561e2b3c6970172e7a9
marlonmoorer <marlonjmoorer@gmail.com>
2018-04-20 19:16:39 -0400

    sanity check

--- src/plugins/vendor.js
+++ src/plugins/vendor.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 //import * as froala from 'froala-editor/js/froala_editor.pkgd.min'
-import * as fontawesome from '~/assets/js/fontawesome-all.min'
+//import * as fontawesome from '~/assets/js/fontawesome-all.min'
 //import VueFroala from 'vue-froala-wysiwyg'
 import VueRichEditor from 'vue-rich-editor';
 //Vue.use(VueFroala)
