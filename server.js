4306289072c474b6324610e70674dc5c92ddfe01
marlonmoorer <marlonjmoorer@gmail.com>
2018-05-06 12:55:39 -0400

    server fix

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
 const {Builder,Nuxt}= require('nuxt')
-const hostile = require('hostile');
-const servers = require('reverse-proxy-mapping');
 let app = express();
 app.use(serveStatic(__dirname));
 
 const port = process.env.PORT || 5000;
-const host = process.env.HOST ||'dev.eaze.com'||'127.0.0.1'
+const host = process.env.HOST ||'127.0.0.1'
 
 
 //process.env.NODE_ENV = 'production';
@@ -24,13 +22,8 @@ app.listen(port,async()=>{
   if (config.dev) {
      await new Builder(nuxt).build()
   }
-  hostile.set('127.0.0.1',host,()=>{
-    servers.add(80,{
-      [host]: `http://127.0.0.1:${port}`,
-    })
-    servers.startAll()
-    console.log(`Server is listening on http://${host}`)
-  })
+  
+  console.log(`Server is listening on http://${host}:${port}`)
 })
 
 
